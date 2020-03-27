a10fb6eda8688d49bfba88a08a2d0ad34aa81548
Shawn <pan.conan@gmail.com>
2020-03-27 20:10:18 +0800

    use v-for, v-bind:key amd uuid module to add NavTabs.vue

---
+++ src/components/NavTabs.vue
@@ -0,0 +1,40 @@
+<template>
+    <ul class="nav nav-tabs mb-4">
+        <li v-for="tab in tabs" v-bind:key="tab.id" li class="nav-item">
+            <router-link v-bind:to="tab.path" class="nav-link">{{tab.title}}</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+import uuid from 'uuid/v4'
+export default {
+    name: 'NavTabs',
+    data: function() {
+        return {
+            tabs: [
+                 {
+                    id: uuid(),
+                    title: '首頁',
+                    path: '/restaurants'
+                },
+                {
+                    id: uuid(),
+                    title: '最新動態',
+                    path: '/restaurants/feeds'
+                },
+                {
+                    id: uuid(),
+                    title: 'Top 10 人氣餐廳',
+                    path: '/restaurants/top'
+                },
+                {
+                    id: uuid(),
+                    title: '美食達人',
+                    path: '/users/top'
+                },
+            ]
+        }
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    name: 'root',
+  },{
     path: '/signin',
     name: 'sign-in',
     component: SignIn
@@ -46,7 +49,8 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  routes
+  linkExactActiveClass: 'active',
+  routes: routes
 })
 
 export default router
