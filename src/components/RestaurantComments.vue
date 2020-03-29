7e1b5e7aa338ebcffce67c0a76ed918688d45290
Shawn <pan.conan@gmail.com>
2020-03-29 12:51:57 +0800

    add delete comment feature for RestaurantComment (use $emit to send data to children's componet and let father's component use v-on to get data from children )

--- src/components/RestaurantComments.vue
+++ src/components/RestaurantComments.vue
@@ -10,6 +10,7 @@
           v-if="currentUser.isAdmin"
           type="button"
           class="btn btn-danger float-right"
+          v-on:click.stop.prevent="handleDeleteButtonClick(comment.id)"
         >
           Delete
         </button>
@@ -52,6 +53,14 @@ export default {
     data: function() {
         return {
             currentUser: dummyUser.currentUser
+        }
+    },
+    methods: {
+        handleDeleteButtonClick (commentId) {
+            console.log('handleDeleteButtonClick', commentId)
+            // TODO: 請求 API 伺服器刪除 id 為 commentId的評論
+            // 觸發父層事件 - $emit('事件名稱', commentId)
+            this.$emit('after-delete-comment', commentId)
         }
     }
 }
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -5,7 +5,7 @@
     <RestaurantDetail v-bind:initial-restaurant="restaurant" />
     <hr>
     <!-- 餐廳評論 RestaurantComments -->
-    <RestaurantComments v-bind:restaurant-comments="restaurantComments" />
+    <RestaurantComments v-bind:restaurant-comments="restaurantComments" v-on:after-delete-comment="afterDeleteComment" />
     <!-- 新增評論 CreateComment -->
   </div>
 </template>
@@ -265,6 +265,12 @@ export default {
                 isLiked: dummyData.isLiked
             }
             this.restaurantComments = dummyData.restaurant.Comments
+        },
+        afterDeleteComment (commentId) {
+            // 以 filter 保留未被選擇的 comment.id
+            this.restaurantComments = this.restaurantComments.filter(
+                comment => comment.id !== commentId
+            )
         }
     },
     components: {
