6a886f1525e2a453d0292883bfa6e5c766de3f10
Shawn <pan.conan@gmail.com>
2020-04-05 22:31:28 +0800

    update /admin/restaurants/{id}/edit to API

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -10,8 +10,18 @@ export default {
         }
     },
     restaurants: {
+        getDetail({restaurantId}) {
+            return apiHelper.get(`/admin/restaurants/${restaurantId}`, {
+                headers: {Authorization: `Bearer ${getToken()}`}
+            })
+        },
         create({formData}) {
             return apiHelper.post('/admin/restaurants', formData, {
+                headers: {Authorization: `Bearer ${getToken()}`}
+            })
+        },
+        update({restaurantId, formData}) {
+            return apiHelper.put(`/admin/restaurants/${restaurantId}`, formData, {
                 headers: {Authorization: `Bearer ${getToken()}`}
             })
         },
